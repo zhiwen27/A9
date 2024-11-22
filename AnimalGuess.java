a57adc7341f8590f7c8ba94a91794c4d6031dcb4
Zhiwen Ji <zji@smith.edu>
2024-11-22 14:33:59 -0500

    need more

--- AnimalGuess.java
+++ AnimalGuess.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 
 public class AnimalGuess{
     public static void main(String[] args) {
+        Boolean play = true;
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
-            
+        while(play){
+            System.err.println("Think of an animal.\n" + "I'll try to guess it.\n" + "Is your animal a Mouse?");
+            String input = sc.next().toLowerCase();
+            if (input.contains("yes")){
+                System.err.println("I guessed it!\n" + "Play again?");
+                input = sc.next().toLowerCase();
+                if (input.contains("no")){
+                    play = false;
+                }
+            }
+            else{
+                System.err.println("I got it wrong.\n" + "Please help me to learn.\n" + "What was your animal?");
+                input = sc.next().toLowerCase();
+            }
         }
     }
 }

6eb813c0cb2bd2dc90cc3f9b35cec73e93fd9cfb
Zhiwen Ji <zji@smith.edu>
2024-11-22 14:22:00 -0500

    2

--- DecisionTree.java
+++ DecisionTree.java
@@ -1,13 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DecisionTree<E> extends BinaryTree<String>{
 
     /** The value at this node */
     private String data;
 
     /** Left child of this node */
-    private BinaryTree<String> left;
+    private DecisionTree<String> left;
 
     /** Right child of this node */
-    private BinaryTree<String> right;
+    private DecisionTree<String> right;
 
     /** This constructor creates a leaf node */
     public DecisionTree(String data){
@@ -15,12 +21,12 @@ public class DecisionTree<E> extends BinaryTree<String>{
     }
 
     /** This constructor creates a branch node */
-    public DecisionTree(String data, BinaryTree<String> left, BinaryTree<String> right){
+    public DecisionTree(String data, DecisionTree<String> left, DecisionTree<String> right){
         super(data, left, right);
     }
 
     /** This constructor creates a deep copy of the entire tree structure */ 
-    public DecisionTree(BinaryTree<String> tree){
+    public DecisionTree(DecisionTree<String> tree){
         super(tree);
     }
 
@@ -30,12 +36,12 @@ public class DecisionTree<E> extends BinaryTree<String>{
     }
 
     /** Accessor for left child */
-    public BinaryTree<String> getLeft() {
+    public DecisionTree<String> getLeft() {
         return left;
     }
 
     /** Accessor for right child */
-    public BinaryTree<String> getRight() {
+    public DecisionTree<String> getRight() {
         return right;
     }
 
@@ -45,22 +51,22 @@ public class DecisionTree<E> extends BinaryTree<String>{
     }
 
     /** Manipulator for left child */
-    public void setLeft(BinaryTree<String> left) {
+    public void setLeft(DecisionTree<String> left) {
         this.left = left;
     }
 
     /** Manipulator for right child */
-    public void setRight(BinaryTree<String> right) {
+    public void setRight(DecisionTree<String> right) {
         this.right = right;
     }
 
     /** Determines whether a tree is empty */
-    public static boolean isEmpty(BinaryTree node) {
+    public static boolean isEmpty(DecisionTree node) {
         return (node == null);
     }
 
-    public BinaryTree<String> followPath(BinaryTree<String> node, String input){
-        BinaryTree<String> returnNode = node;
+    public static DecisionTree<String> followPath(DecisionTree<String> node, String input){
+        DecisionTree<String> returnNode = node;
         for(int i = 0; i < input.length(); i++){
             if (input.charAt(i) == 'Y'){
                 returnNode = returnNode.getLeft();
@@ -70,5 +76,38 @@ public class DecisionTree<E> extends BinaryTree<String>{
             }
         }
         return returnNode;
+    }
+
+    public static void readFile(String name){
+        Scanner file = null;
+        try {
+          file = new Scanner(new File(name));
+        } catch (FileNotFoundException e) {
+          System.err.println("Cannot locate file.");
+          System.exit(-1);
+        }
+        ArrayList<String> inputLine = new ArrayList<>();
+        while (file.hasNextLine()){
+            inputLine.add(file.nextLine());
+        }
+        DecisionTree<String> initialTree = new DecisionTree<>(inputLine.get(0));
+        // for(int i = 1; i < inputLine.size(); i++){
+            
+        // }
+        System.err.println(inputLine.get(0));
+    }
+
+    public static void main(String[] args) {
+        String inputLine = "";
+        if (args.length == 0) {
+            System.err.println("Usage:  java Postfix <expr>");
+        } else {
+            Scanner sc = new Scanner(new StringReader(args[0]));
+            while (sc.hasNext()) {
+                inputLine = sc.next();
+            }
+            sc.close();
+        }
+        DecisionTree.readFile(inputLine);
     }
 }

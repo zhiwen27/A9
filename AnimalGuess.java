f33d4e66e98fb1b4c7bdc43924c2989ae20eeb8e
Zhiwen Ji <zji@smith.edu>
2024-11-26 21:59:37 -0500

    save

--- AnimalGuess.java
+++ AnimalGuess.java
@@ -34,86 +34,137 @@ public class AnimalGuess{
             }
             sc.close();
         }
-        DecisionTree<String> tree = DecisionTree.readFile(inputLine);
-        Boolean play = true;
+        Boolean playOut = true;
         Scanner sc = new Scanner(System.in);
-        System.err.println("Think of an animal.\n" + "I'll try to guess it.\n" + tree.getData().replaceFirst(" ", ""));
-        String input = sc.nextLine().toLowerCase();
-        while(play){
-            if (AnimalGuess.checkYes(input)) {
-                tree = (DecisionTree<String>) tree.getLeft();
-                if ((tree.isBranch())){
-                    System.err.println(tree.getData());
-                    input = sc.nextLine().toLowerCase();
-                }
-                else{
-                    System.err.println("Is it a " + tree.getData() + "?");
-                    input = sc.nextLine().toLowerCase();
-                    if (AnimalGuess.checkYes(input)){
-                        System.err.println("I guessed it!\n" + "Play again?");
+        
+        while(playOut){
+            DecisionTree<String> tree = DecisionTree.readFile(inputLine);
+            DecisionTree<String> pointer = tree;
+            System.err.println("Think of an animal.\n" + "I'll try to guess it.\n" + tree.getData().replaceFirst(" ", ""));
+            String input = sc.nextLine().toLowerCase();
+            Boolean playIn = true;
+            while(playIn){
+                if ((AnimalGuess.checkYes(input)) && (playIn)) {
+                    // tree = (DecisionTree<String>) tree.getLeft();
+                    DecisionTree<String> treeTemp = (DecisionTree<String>) tree.getLeft();
+                    tree = (DecisionTree<String>) tree.getLeft();
+                    if ((treeTemp.isBranch())){
+                        System.err.println(treeTemp.getData());
                         input = sc.nextLine().toLowerCase();
-                        if (AnimalGuess.checkNo(input)){
-                            play = false;
-                        }
                     }
-                    else if (AnimalGuess.checkNo(input)){
-                        System.err.println("I got it wrong.\n" + "Please help me to learn.\n" + "What was your animal?");
-                        input = sc.nextLine();
-                        String nodeName = input;
-                        System.err.println("Type a yes or no question that would distinguish between a " + nodeName + " and a " + tree.getData() + "?");
-                        input = sc.nextLine();
-                        DecisionTree<String> addNode = new DecisionTree<>(input);
-                        System.err.println("Would you answer yes to this question for the " + nodeName + "?");
+                    else{
+                        System.err.println("Is it a " + treeTemp.getData() + "?");
                         input = sc.nextLine().toLowerCase();
                         if (AnimalGuess.checkYes(input)){
-                            addNode.setLeft(new DecisionTree<String>(nodeName));
-                            addNode.setRight(new DecisionTree<String>(tree));
+                            System.err.println("I guessed it!\n" + "Play again?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                //playIn = false;
+                                break;
+                            }
+                            else{
+                                playIn = false;
+                                playOut = false;
+                            }
                         }
-                        else{
-                            addNode.setLeft(new DecisionTree<String>(tree));
-                            addNode.setRight(new DecisionTree<String>(nodeName));
+                        else if (AnimalGuess.checkNo(input)){
+                            System.err.println("I got it wrong.\n" + "Please help me to learn.\n" + "What was your animal?");
+                            input = sc.nextLine();
+                            String nodeName = input;
+                            System.err.println("Type a yes or no question that would distinguish between a " + nodeName + " and a " + treeTemp.getData() + "?");
+                            input = sc.nextLine();
+                            DecisionTree<String> addNode = new DecisionTree<>(input);
+                            System.err.println("Would you answer yes to this question for the " + nodeName + "?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                addNode.setLeft(new DecisionTree<String>(nodeName));
+                                addNode.setRight(new DecisionTree<String>(treeTemp));
+                                tree.setLeft(addNode);;
+                            }
+                            else{
+                                addNode.setLeft(new DecisionTree<String>(treeTemp));
+                                addNode.setRight(new DecisionTree<String>(nodeName));
+                                tree.setLeft(addNode);;
+                            }
+                            System.err.println(DecisionTree.preorderString(pointer));
+                            System.err.println("Play again?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                // playIn = false;
+                                break;
+                            }
+                            else{
+                                playIn = false;
+                                playOut = false;
+                            }
                         }
-                        //tree = addNode;
+                        tree = (DecisionTree<String>) tree.getRight();
                     }
+                    // if (playIn){
+                    //     tree = (DecisionTree<String>) tree.getLeft();
+                    // }
                 }
-            }
-            else{
-                tree = (DecisionTree<String>) tree.getRight();
-                if ((tree.isBranch())){
-                    System.err.println(tree.getData());
-                    input = sc.nextLine().toLowerCase();
-                }
-                else{
-                    System.err.println("Is it a " + tree.getData() + "?");
-                    input = sc.nextLine().toLowerCase();
-                    if (AnimalGuess.checkYes(input)){
-                        System.err.println("I guessed it!\n" + "Play again?");
+                else if ((AnimalGuess.checkNo(input)) && (playIn)){
+                    // tree = (DecisionTree<String>) tree.getRight();
+                    DecisionTree<String> treeTemp = (DecisionTree<String>) tree.getRight();
+                    tree = (DecisionTree<String>) tree.getRight();
+                    if ((treeTemp.isBranch())){
+                        System.err.println(treeTemp.getData());
                         input = sc.nextLine().toLowerCase();
-                        if (AnimalGuess.checkNo(input)){
-                            play = false;
-                        }
                     }
-                    else if (AnimalGuess.checkNo(input)){
-                        System.err.println("I got it wrong.\n" + "Please help me to learn.\n" + "What was your animal?");
-                        input = sc.nextLine();
-                        String nodeName = input;
-                        System.err.println("Type a yes or no question that would distinguish between a " + nodeName + " and a " + tree.getData() + "?");
-                        input = sc.nextLine();
-                        DecisionTree<String> addNode = new DecisionTree<>(input);
-                        System.err.println("Would you answer yes to this question for the " + nodeName + "?");
+                    else{
+                        System.err.println("Is it a " + treeTemp.getData() + "?");
                         input = sc.nextLine().toLowerCase();
                         if (AnimalGuess.checkYes(input)){
-                            addNode.setLeft(new DecisionTree<String>(nodeName));
-                            addNode.setRight(new DecisionTree<String>(tree));
+                            System.err.println("I guessed it!\n" + "Play again?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                // playIn = false;
+                                break;
+                            }
+                            else{
+                                playIn = false;
+                                playOut = false;
+                            }
                         }
-                        else{
-                            addNode.setLeft(new DecisionTree<String>(tree));
-                            addNode.setRight(new DecisionTree<String>(nodeName));
+                        else if (AnimalGuess.checkNo(input)){
+                            System.err.println("I got it wrong.\n" + "Please help me to learn.\n" + "What was your animal?");
+                            input = sc.nextLine();
+                            String nodeName = input;
+                            System.err.println("Type a yes or no question that would distinguish between a " + nodeName + " and a " + treeTemp.getData() + "?");
+                            input = sc.nextLine();
+                            DecisionTree<String> addNode = new DecisionTree<>(input);
+                            System.err.println("Would you answer yes to this question for the " + nodeName + "?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                addNode.setLeft(new DecisionTree<String>(nodeName));
+                                addNode.setRight(new DecisionTree<String>(treeTemp));
+                                tree.setRight(addNode);;
+                            }
+                            else{
+                                addNode.setLeft(new DecisionTree<String>(treeTemp));
+                                addNode.setRight(new DecisionTree<String>(nodeName));
+                                tree.setRight(addNode);;
+                            }
+                            System.err.println(DecisionTree.preorderString(pointer));
+                            System.err.println("Play again?");
+                            input = sc.nextLine().toLowerCase();
+                            if (AnimalGuess.checkYes(input)){
+                                // playIn = false;
+                                break;
+                            }
+                            else{
+                                playIn = false;
+                                playOut = false;
+                            }
                         }
-                        //tree = addNode;
                     }
+                    // if (playIn){
+                    //     tree = (DecisionTree<String>) tree.getRight();
+                    // }
                 }
             }
+            // traverse through the tree the write the node value into a new file with path indication
         }
         sc.close();
     }
